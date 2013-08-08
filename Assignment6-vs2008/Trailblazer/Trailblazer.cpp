716c3a6f572c041ff329ab319b81357963071685
Yotta2 <cloudsweet1989@gmail.com>
2013-08-07 22:48:44 -0400

    Assignment 6: finished Dijkstra and A*

--- Assignment6-vs2008/Trailblazer/Trailblazer.cpp
+++ Assignment6-vs2008/Trailblazer/Trailblazer.cpp
@@ -11,6 +11,52 @@
 #include "TrailblazerPQueue.h"
 using namespace std;
 
+#define P 1e-6
+
+void init(Loc &start,
+		  Grid<Loc> &parent,
+		  Grid<Color> &cellColor,
+		  Grid<double> &dist,
+		  Grid<double>& world) {
+	parent.resize(world.nRows, world.nCols);
+	cellColor.resize(world.nRows, world.nCols);
+	dist.resize(world.nRows, world.nCols);
+	for (int i = 0; i < world.nRows; i++)
+		for (int j = 0; j < world.nCols; j++)
+			cellColor[i][j] = GRAY;
+	dist[start.row][start.col] = 0;
+}
+
+bool isOutOfBounds(int rNext, int cNext, int row, int col) {
+	if (rNext < 0 || rNext >= row)
+		return true;
+	if (cNext < 0 || cNext >= col)
+		return true;
+	return false;
+}
+
+void reverse(Vector<Loc> &path) {
+	Vector<Loc> result;
+	for (int i = path.size() - 1; i >= 0; i--)
+		result.push_back(path[i]);
+	path = result;
+}
+
+void getPath(Loc start,
+             Loc end,
+			 Vector<Loc> &path, 
+			 Grid<Loc> &parent) {
+	Loc loc = end;
+	path.push_back(loc);
+	while (true) {
+		loc = parent[loc.row][loc.col];
+		path.push_back(loc);
+		if (loc == start)
+			break;
+	}
+	reverse(path);
+}
+
 /* Function: shortestPath
  * 
  * Finds the shortest path between the locations given by start and end in the
@@ -29,10 +75,52 @@ Vector<Loc>
 shortestPath(Loc start,
              Loc end,
              Grid<double>& world,
-             double costFn(Loc from, Loc to, Grid<double>& world)) {
+             double costFn(Loc from, Loc to, Grid<double>& world),
+			 double heuristic(Loc start, Loc end, Grid<double>& world)) {
 	// TODO: Fill this in!
-	error("shortestPath is not implemented yet.");
-    return Vector<Loc>();
+	//error("shortestPath is not implemented yet.");
+	Grid<Loc> parent;
+	Grid<Color> cellColor;
+	Grid<double> dist;
+	int dir[8][2] = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+	Vector<Loc> path;
+	TrailblazerPQueue<Loc> pqueue;
+	init(start, parent, cellColor, dist, world);
+	cellColor[start.row][start.col] = YELLOW;
+	colorCell(world, start, YELLOW);
+	parent[start.row][start.col] = start;
+	pqueue.enqueue(start, heuristic(start, end, world));
+	while (!pqueue.isEmpty()) {
+		Loc curr = pqueue.dequeueMin();
+		cellColor[curr.row][curr.col] = GREEN;
+		colorCell(world, curr, GREEN);
+		if (curr == end)
+			break;
+		for (int i = 0; i < 8; i++) {
+			int rNext = curr.row + dir[i][0];
+			int cNext = curr.col + dir[i][1];
+			if (isOutOfBounds(rNext, cNext, world.nRows, world.nCols))
+				continue;
+			Loc next;
+			next.row = rNext;
+			next.col = cNext;
+			double L = costFn(curr, next, world);
+			if (cellColor[rNext][cNext] == GRAY) {
+				cellColor[rNext][cNext] = YELLOW;
+				colorCell(world, next, YELLOW);
+				dist[rNext][cNext] = dist[curr.row][curr.col] + L;
+				parent[rNext][cNext] = curr;
+				pqueue.enqueue(next, dist[rNext][cNext] + heuristic(next, end, world));
+			} else if (cellColor[rNext][cNext] == YELLOW &&
+					dist[rNext][cNext] - (dist[curr.row][curr.col] + L) > P) {
+				dist[rNext][cNext] = dist[curr.row][curr.col] + L;
+				parent[rNext][cNext] = curr;
+				pqueue.decreaseKey(next, dist[rNext][cNext] + heuristic(next, end, world));
+			}
+		}
+	}
+	getPath(start, end, path, parent);
+    return path;
 }
 
 Set<Edge> createMaze(int numRows, int numCols) {
